19dc8ec3810643c7fd8d552ae460a7da7410033c
maximilionus <maximilionuss@gmail.com>
2023-11-28 20:42:43 +0300

    Tweak stem darkening values
    Tweaked stem darkening values should preserve the original font weight
    difference visibility.

--- src/profile.d/freetype-macify.sh
+++ src/profile.d/freetype-macify.sh
@@ -1,2 +1,2 @@
 # Make fonts look bolder and easier to read by trying to mimic the MacOS rendering style.
-export FREETYPE_PROPERTIES="autofitter:no-stem-darkening=0 autofitter:darkening-parameters=500,0,1000,400,1500,350,1700,0"
+export FREETYPE_PROPERTIES="autofitter:no-stem-darkening=0 autofitter:darkening-parameters=500,0,1000,400,1250,250,1500,0"
