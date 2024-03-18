f73328963763abde713d452371004449811c2a20
maximilionus <maximilionuss@gmail.com>
2024-03-18 23:41:39 +0300

    Add exp. stem-darkening for cff (read msg below!)
    cff is heavily used in Gnome DE and requires additional tweak from user.
    You should disable the Gnome font hinting by executing the next command:
    
    gsettings set org.gnome.desktop.interface font-hinting none
    
    To revert the change, execute:
    gsettings reset org.gnome.desktop.interface font-hinting

--- src/profile.d/freetype-envision-safe.sh
+++ src/profile.d/freetype-envision-safe.sh
@@ -5,4 +5,5 @@
 # Safe version doesn't embolden very small font sizes to exclude
 # icons emboldening issue.
 export FREETYPE_PROPERTIES="autofitter:no-stem-darkening=0\
- autofitter:darkening-parameters=500,0,1000,400,1250,250,1500,0"
+ autofitter:darkening-parameters=500,0,1000,400,1250,250,1500,0\
+ cff:no-stem-darkening=0"
