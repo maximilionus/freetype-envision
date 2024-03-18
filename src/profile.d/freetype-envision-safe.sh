e19bcdf7922d3fafd4fb6a32359d67fcec94de43
maximilionus <maximilionuss@gmail.com>
2024-03-18 21:07:56 +0300

    profile.d: Multiline split, enhance docs

--- src/profile.d/freetype-envision-safe.sh
+++ src/profile.d/freetype-envision-safe.sh
@@ -1,2 +1,10 @@
-# Make fonts look bolder and easier to read by trying to mimic the MacOS rendering style.
-export FREETYPE_PROPERTIES="autofitter:no-stem-darkening=0 autofitter:darkening-parameters=500,0,1000,400,1250,250,1500,0"
+# Embolden fonts to make them easier to read (mimic the macOS rendering style).
+# Official docs:
+# - https://freetype.org/freetype2/docs/reference/ft2-properties.html#no-stem-darkening
+#
+# Safe version doesn't embolden very small font sizes to exclude
+# icons emboldening issue.
+export FREETYPE_PROPERTIES=" \
+    autofitter:no-stem-darkening=0 \
+    autofitter:darkening-parameters=500,0,1000,400,1250,250,1500,0 \
+"
