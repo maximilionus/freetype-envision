41e2285d5b3cfbdc1c430741fab8f43367bbdee7
maximilionus <maximilionuss@gmail.com>
2024-03-19 01:19:05 +0300

    Tweak config.sh, bump version to 0.1.1, changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,7 @@
+## Release 0.1.1
+### Tweaked
+- Improve the structure of profile.d scripts, add comment blocks describing actions.
+
+
 ## Release 0.1.0
 Initial release of this project.
--- config.sh
+++ config.sh
@@ -4,8 +4,9 @@ PRESET=${PRESET:-safe}
 # Uncomment the line below to enable the unsafe preset.
 #PRESET=unsafe
 
+VERSION="0.1.1"
+
 SRC_DIR=src
-VERSION="0.1.0"
 PROFILED_DIR="$SRC_DIR/profile.d"
 PROFILED_SAFE="freetype-envision-safe.sh"
 PROFILED_UNSAFE="freetype-envision-unsafe.sh"
