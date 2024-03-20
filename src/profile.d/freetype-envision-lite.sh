c887a452a3e90bcea87726b38a3efaa088fcdbb7
maximilionus <maximilionuss@gmail.com>
2024-03-21 00:03:18 +0300

    Fix profile.d lite var assign syntax

--- src/profile.d/freetype-envision-lite.sh
+++ src/profile.d/freetype-envision-lite.sh
@@ -3,11 +3,10 @@
 # - https://freetype.org/freetype2/docs/reference/ft2-properties.html#no-stem-darkening
 #
 # Lite version DOES NOT:
-# - Disable the font hinting.
 # - Embolden very small font sizes
 #   (to exclude icons emboldening issue).
 export FREETYPE_PROPERTIES=\
- "autofitter:no-stem-darkening=0\
- autofitter:darkening-parameters=500,0,1000,400,1250,250,1500,0\
+"autofitter:no-stem-darkening=0\
+ autofitter:darkening-parameters=500,0,1000,400,1500,250,2000,0\
  type1:no-stem-darkening=0\
  t1cid:no-stem-darkening=0"
